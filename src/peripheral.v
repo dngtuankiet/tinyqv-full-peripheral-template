e596d06af807dada68f24d0673e5570b3b882299
dngtuankiet <dngtuankiet97@gmail.com>
2025-08-24 12:54:36 +0900

    update branch for gds test

--- src/peripheral.v
+++ src/peripheral.v
@@ -8,7 +8,7 @@
 // Change the name of this module to something that reflects its functionality and includes your name for uniqueness
 // For example tqvp_yourname_spi for an SPI peripheral.
 // Then edit tt_wrapper.v line 41 and change tqvp_example to your chosen module name.
-module tqvp_example (
+module trng_kietdang (
     input         clk,          // Clock - the TinyQV project clock is normally set to 64MHz.
     input         rst_n,        // Reset_n - low to reset.
 
@@ -30,30 +30,102 @@ module tqvp_example (
 
     output        user_interrupt  // Dedicated interrupt request for this peripheral
 );
-
     // Implement a 32-bit read/write register at address 0
-    reg [31:0] example_data;
+    reg [31:0] r_data_addr_0; // CONTROL REGISTER[3:0] -> READ[4] | CALIB[3] | SEL_BASE[2] | EN[1] | RST[0]
+    reg [31:0] r_data_addr_1; // STATUS REGISTER
+    reg [31:0] r_data_addr_2; // CALIBRATION CYCLES REGISTER
+    reg [31:0] r_data_addr_3; // I1 REGISTER
+    reg [31:0] r_data_addr_4; // I2 REGISTER
+    reg [31:0] r_data_addr_5; // TRIGGER REGISTER
+    reg [31:0] r_data_addr_6; // CLK DIVISION REGISTER
+
+
+    // Instantiate the dual_trng module
+    wire [31:0] random_number;
+    wire ready_signal;
+    dual_trng trng_inst (
+        .iClk(clk),
+        .iRst(~rst_n | r_data_addr_0[0]),     // Reset if rst_n is low or RST bit is set
+        .iEn(r_data_addr_0[1]),               // Enable controlled by EN bit
+        .iClk_div_factor(r_data_addr_6),       // Clock division factor
+        .iCalib(r_data_addr_0[3]),            // Calibration control
+        .iCalib_cycles(r_data_addr_2),        // Calibration cycles
+        .iTrigger(r_data_addr_5[23:0]),       // Trigger input
+        .iI1(r_data_addr_3[23:0]),            // I1 input
+        .iI2(r_data_addr_4[23:0]),            // I2 input
+        .iSel_base(r_data_addr_0[2]),         // Select long base (0 for long, 1 for short)
+        .iRead(r_data_addr_0[4]),             // Read request
+        .oReady(ready_signal),                // Ready signal
+        .oRandom(random_number)               // Random number output
+    );
+
+    // Input handling for the registers
     always @(posedge clk) begin
         if (!rst_n) begin
-            example_data <= 0;
+            r_data_addr_0 <= 0;
+            r_data_addr_1 <= 0;
+            r_data_addr_2 <= 0;
+            r_data_addr_3 <= 0;
+            r_data_addr_4 <= 0;
+            r_data_addr_5 <= 0;
+            r_data_addr_6 <= 1; // Default clock division factor
         end else begin
             if (address == 6'h0) begin
-                if (data_write_n != 2'b11)              example_data[7:0]   <= data_in[7:0];
-                if (data_write_n[1] != data_write_n[0]) example_data[15:8]  <= data_in[15:8];
-                if (data_write_n == 2'b10)              example_data[31:16] <= data_in[31:16];
+                if (data_write_n != 2'b11)              r_data_addr_0[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) r_data_addr_0[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              r_data_addr_0[31:16] <= data_in[31:16];
+            end
+            if (address == 6'h1) begin
+                if (data_write_n != 2'b11)              r_data_addr_1[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) r_data_addr_1[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              r_data_addr_1[31:16] <= data_in[31:16];
+            end
+            if (address == 6'h2) begin
+                if (data_write_n != 2'b11)              r_data_addr_2[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) r_data_addr_2[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              r_data_addr_2[31:16] <= data_in[31:16];
+            end
+            if (address == 6'h3) begin
+                if (data_write_n != 2'b11)              r_data_addr_3[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) r_data_addr_3[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              r_data_addr_3[31:16] <= data_in[31:16];
+            end
+            if (address == 6'h4) begin
+                if (data_write_n != 2'b11)              r_data_addr_4[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) r_data_addr_4[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              r_data_addr_4[31:16] <= data_in[31:16];
+            end
+            if (address == 6'h5) begin
+                if (data_write_n != 2'b11)              r_data_addr_5[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) r_data_addr_5[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              r_data_addr_5[31:16] <= data_in[31:16];
+            end
+            if (address == 6'h6) begin
+                if (data_write_n != 2'b11)              r_data_addr_6[7:0]   <= data_in[7:0];
+                if (data_write_n[1] != data_write_n[0]) r_data_addr_6[15:8]  <= data_in[15:8];
+                if (data_write_n == 2'b10)              r_data_addr_6[31:16] <= data_in[31:16];
             end
         end
     end
 
+
+
+
     // The bottom 8 bits of the stored data are added to ui_in and output to uo_out.
-    assign uo_out = example_data[7:0] + ui_in;
+    assign uo_out = r_data_addr_0[7:0] + ui_in;
 
     // Address 0 reads the example data register.  
     // Address 4 reads ui_in
     // All other addresses read 0.
-    assign data_out = (address == 6'h0) ? example_data :
-                      (address == 6'h4) ? {24'h0, ui_in} :
-                      32'h0;
+    assign data_out = (address == 6'h0) ? r_data_addr_0 :
+                    (address == 6'h1) ? {r_data_addr_1[31:1], ready_signal} :
+                    (address == 6'h2) ? r_data_addr_2 :
+                    (address == 6'h3) ? r_data_addr_3 :
+                    (address == 6'h4) ? r_data_addr_4 :
+                    (address == 6'h5) ? r_data_addr_5 :
+                    (address == 6'h6) ? r_data_addr_6 :
+                    (address == 6'h7) ? random_number :
+                    32'h0;
 
     // All reads complete in 1 clock
     assign data_ready = 1;
--- test/test.py
+++ test/test.py
@@ -7,10 +7,7 @@ from cocotb.triggers import ClockCycles
 
 from tqv import TinyQV
 
-# When submitting your design, change this to the peripheral number
-# in peripherals.v.  e.g. if your design is i_user_peri05, set this to 5.
-# The peripheral number is not used by the test harness.
-PERIPHERAL_NUM = 0
+PERIPHERAL_NUM = 30
 
 @cocotb.test()
 async def test_project(dut):
@@ -23,55 +20,141 @@ async def test_project(dut):
     # Interact with your design's registers through this TinyQV class.
     # This will allow the same test to be run when your design is integrated
     # with TinyQV - the implementation of this class will be replaces with a
-    # different version that uses Risc-V instructions instead of the SPI test
-    # harness interface to read and write the registers.
+    # different version that uses Risc-V instructions instead of the SPI 
+    # interface to read and write the registers.
     tqv = TinyQV(dut, PERIPHERAL_NUM)
 
     # Reset
     await tqv.reset()
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test TRNG peripheral behavior")
+    # Register addresses
+    CONTROL_REG = 0x0
+    STATUS_REG = 0x1
+    CALIBRATION_CYCLES_REG = 0x2
+    I1_REG = 0x3
+    I2_REG = 0x4
+    TRIGGER_REG = 0x5
+    CLK_DIVISION_REG = 0x6 #TinyQV runs at 64MHz, it is nice to have a clock divider to run at lower frequencies
+    RANDOM_NUMBER_REG = 0x7
 
-    # Test register write and read back
-    await tqv.write_word_reg(0, 0x82345678)
-    assert await tqv.read_byte_reg(0) == 0x78
-    assert await tqv.read_hword_reg(0) == 0x5678
-    assert await tqv.read_word_reg(0) == 0x82345678
+    # Control register bits
+    RESET = 0x1 << 0
+    CORE_ENABLE = 0x1 << 1
+    SEL_BASE_SHORT = 0x1 << 2
+    CALIBRATION = 0x1 << 3
+    READ_REQUEST = 0x1 << 4
+    READY = 0x1 << 0
 
-    # Set an input value, in the example this will be added to the register value
-    dut.ui_in.value = 30
+    # Parameters for the TRNG peripheral
+    # These values can be adjusted based on the test case
+    ENTROPY_CELLS = 0x1 # 24-bit entropy cells
+    SELECT_BASE_SHORT = False # Set to True to select the short base, False for the long base
 
-    # Wait for two clock cycles to see the output values, because ui_in is synchronized over two clocks,
-    # and a further clock is required for the output to propagate.
-    await ClockCycles(dut.clk, 3)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 0x96
-
-    # Input value should be read back from register 1
-    assert await tqv.read_byte_reg(4) == 30
-
-    # Zero should be read back from register 2
-    assert await tqv.read_word_reg(8) == 0
-
-    # A second write should work
-    await tqv.write_word_reg(0, 40)
-    assert dut.uo_out.value == 70
-
-    # Test the interrupt, generated when ui_in[6] goes high
-    dut.ui_in[6].value = 1
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in[6].value = 0
-
-    # Interrupt asserted
-    await ClockCycles(dut.clk, 3)
-    assert await tqv.is_interrupt_asserted()
-
-    # Interrupt doesn't clear
-    await ClockCycles(dut.clk, 10)
-    assert await tqv.is_interrupt_asserted()
+    #=== Step 1: Reset TRNG peripheral ===#
+    dut._log.info("----------------------------------")
+    dut._log.info("Step 1: Reset TRNG peripheral")
+    await tqv.write_word_reg(CONTROL_REG, RESET | CORE_ENABLE) # Reset and enable the TRNG peripheral
+    await ClockCycles(dut.clk, 1000) # Reset the TRNG for 1000 clock cycles
+    await tqv.write_word_reg(CONTROL_REG, CORE_ENABLE)
+    await tqv.write_word_reg(CLK_DIVISION_REG, 0x10) # Set clock division factor to 16 
     
-    # Write bottom bit of address 8 high to clear
-    await tqv.write_byte_reg(8, 1)
-    assert not await tqv.is_interrupt_asserted()
+    control_reg = await tqv.read_word_reg(CONTROL_REG)
+    dut._log.info(f"TRNG control register after reset: {control_reg:#x}")
+    status_reg = await tqv.read_word_reg(STATUS_REG)
+    dut._log.info(f"TRNG status register after reset: {status_reg:#x}")
+
+    #=== Step 2: Trigger entropy cells and select ring generator base ===#
+    dut._log.info("----------------------------------")
+    dut._log.info("Step 2: Trigger entropy cells")
+    await tqv.write_word_reg(I1_REG, ENTROPY_CELLS)
+    await tqv.write_word_reg(I2_REG, 0x0) # opposite of I1_REG
+    await tqv.write_word_reg(TRIGGER_REG, ENTROPY_CELLS)
+    await ClockCycles(dut.clk, 10) # Wait for the trigger to take effect
+    dut._log.info(f"Triggered entropy cells: {ENTROPY_CELLS:#x}")
+
+    # Select the ring generator base
+    if(SELECT_BASE_SHORT):
+        dut._log.info("Selecting ring generator base short")
+        control_reg = await tqv.read_word_reg(CONTROL_REG)
+        await tqv.write_word_reg(CONTROL_REG, SEL_BASE_SHORT | control_reg)
+    else:
+        dut._log.info("Default selecing ring generator base long")
+ 
+
+    #=== Step 3: Calibrate TRNG peripheral ===#
+    dut._log.info("----------------------------------")
+    dut._log.info("Step 3: Calibrate TRNG peripheral")
+    
+    # Set the number of calibration cycles
+    calibration_cycles = 0x1 << 11  # Example: 2^11 cycles
+    await tqv.write_word_reg(CALIBRATION_CYCLES_REG, calibration_cycles)  # Set calibration cycles
+    cycle_reg = await tqv.read_word_reg(CALIBRATION_CYCLES_REG) # read out the value to verify and print it
+    assert cycle_reg == calibration_cycles
+    dut._log.info(f"Set calibration cycle: {calibration_cycles} - Read out calibration cycles: {cycle_reg}")    
+    
+    # Trigger calibration
+    control_reg = await tqv.read_word_reg(CONTROL_REG)
+    await tqv.write_word_reg(CONTROL_REG, CALIBRATION | control_reg)
+    
+    timeout = calibration_cycles*2  # Set a timeout for calibration
+    status_reg = await tqv.read_word_reg(STATUS_REG)
+    # if the calibration cycles is too short, the ready signal will asserted
+    dut._log.info(f"TRNG status before loop: {status_reg:#x}") 
+    # Wait for ready signal to be asserted
+    while (await tqv.read_word_reg(STATUS_REG) & READY) == 0:
+        status_reg = await tqv.read_word_reg(STATUS_REG)
+        # dut._log.info(f"TRNG status during calibration: {status_reg:#x}")
+        await ClockCycles(dut.clk, 1)
+        timeout -= 1
+        if timeout <= 0:
+            raise TimeoutError("TRNG calibration timed out")
+    
+    dut._log.info("TRNG peripheral is ready after calibration")
+
+    #=== Step 4: Read TRNG output ===#
+    dut._log.info("----------------------------------")
+    dut._log.info("Step 4: Read TRNG output")
+    control_reg = await tqv.read_word_reg(CONTROL_REG)
+    await tqv.write_word_reg(CONTROL_REG, READ_REQUEST | control_reg) 
+
+    # Maybe the time out is not necessary here
+    # By the time we reach here, the ready signal should be asserted
+    timeout = 32*2  # The ready signal should be asserted within 32 cycles for reading out 32-bit random data
+    while (await tqv.read_word_reg(STATUS_REG) & READY) == 0:
+        # status_reg = await tqv.read_word_reg(STATUS_REG)
+        await ClockCycles(dut.clk, 1)
+        timeout -= 1
+        if timeout <= 0:
+            raise TimeoutError("TRNG read request timed out") 
+    dut._log.info("TRNG read request completed")
+
+    # Read the random data
+    random_data = await tqv.read_word_reg(RANDOM_NUMBER_REG)
+    # Deassert the read request
+    control_reg = await tqv.read_word_reg(CONTROL_REG)
+    await tqv.write_word_reg(CONTROL_REG, control_reg & ~READ_REQUEST)
+    dut._log.info(f"TRNG random data: 0x{random_data:08x}")
+
+    #=== Step 5: Repeat to read several random numbers ===#
+    dut._log.info("----------------------------------")
+    dut._log.info("Step 5: Repeat to read several random numbers")
+    for i in range(5):
+        control_reg = await tqv.read_word_reg(CONTROL_REG)
+        await tqv.write_word_reg(CONTROL_REG, READ_REQUEST | control_reg)
+
+        # Again, maybe the time out is not necessary here
+        timeout = 32*2  # The ready signal should be asserted within 32 cycles for reading out 32-bit random data
+        while (await tqv.read_word_reg(STATUS_REG) & READY) == 0:
+            # status_reg = await tqv.read_word_reg(STATUS_REG)
+            await ClockCycles(dut.clk, 1)
+            timeout -= 1
+            if timeout <= 0:
+                raise TimeoutError("TRNG read request timed out") 
+
+        random_data = await tqv.read_word_reg(RANDOM_NUMBER_REG) # Read the random data
+        control_reg = await tqv.read_word_reg(CONTROL_REG) # Deassert the read request
+
+        await tqv.write_word_reg(CONTROL_REG, control_reg & ~READ_REQUEST)
+        dut._log.info(f"TRNG random data {i+1}: 0x{random_data:08x}")
+    dut._log.info("Test completed successfully")
